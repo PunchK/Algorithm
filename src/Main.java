57af64da50aca6451407495b3922687fbcda2575
sec <1114732025@qq.com>
2022-01-12 11:18:32 +0800

    .插入排序-InsertSort；5.快速排序-QuickSort；6.选择排序-SelectSort；7.希尔排序-SellSort；8.基数排序-BasicSort

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) throws IOException
     {
         SortFunction sortFunction = new SortFunction();
-        //1.归并排序测试-MergeSort；2.堆排序-HeapSort；
-        sortFunction.chooseFunc("MergeSort");
+        /*1.归并排序测试-MergeSort；2.堆排序-HeapSort；3.冒泡排序-BubbleSort；4.插入排序-InsertSort；5.快速排序-QuickSort；6.选择排序-SelectSort；
+        7.希尔排序-SellSort；8.基数排序-BasicSort*/
+        sortFunction.chooseFunc("BasicSort");
     }
 }
